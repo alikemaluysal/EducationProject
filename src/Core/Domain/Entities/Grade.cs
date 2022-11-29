b12d8599ae8f4f7723defe9e25f90aab755ecd89
Mehmet Ali <mehmetaliozdemira@gmail.com>
2022-11-29 22:13:18 +0300

    Entities

---
+++ src/Core/Domain/Entities/Grade.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.Entities
+{
+    public class Grade
+    {
+        public string ExamNote { get; set; }
+        //enum?
+        //public Course Course { get; set; }
+        //public Exam Exam { get; set; }
+
+    }
+}
--- src/Core/Domain/Entities/Student.cs
+++ src/Core/Domain/Entities/Student.cs
@@ -1,16 +1,14 @@
 namespace Domain.Entities
 {
-    public class Student 
+    public class Student
     {
-        public int StudentNo {get; set;}
-       public string Name {get; set;}
-       public string Address {get; set;}
-       public string Mobile
+        //public Department Department { get; set; }
+        public int StudentNo { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string MobileNumber { get; set; }
+        public ICollection<string> ExamGrades { get; set; }
+        //grade entity
 
-       public string ParentName {get; set;}
-       //parent entity
-        public ICollection<string> ExamGrades {get; set;}
-         //note entity
-      
     }
 }
