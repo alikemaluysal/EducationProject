4cf883169474eba8791d2789f830d0cf9da9ba89
Ali Kemal Uysal <alikemaluysal4235@gmail.com>
2022-11-29 22:40:54 +0300

    Entities

---
+++ src/Core/Domain/Entities/Person.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Person : Entity
+    {
+        public int FirstName { get; set; }
+        public int LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+    }
+}
--- src/Core/Domain/Entities/Teaches.cs
+++ src/Core/Domain/Entities/Teaches.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Domain.Entities
 {
-    internal class Class1
+    public class Teaches
     {
     }
 }
