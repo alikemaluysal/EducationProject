2821666d5028a9c51bcb73d492539a7c1a8f2f42
Mehmet Ali <mehmetaliozdemira@gmail.com>
2023-01-24 00:22:30 +0300

    connectionStringConfiguration class

---
+++ src/Infrastructure/Persistence/Configuration/ConnectionStringConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Persistence.Configuration
+{
+    static class ConnectionStringConfiguration
+    {
+        public static string connectionString
+        {
+            get
+            {
+                ConfigurationManager configurationManager = new();
+                configurationManager.SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), "../../../Presentation/API/API"));
+                configurationManager.AddJsonFile("appsettings.json");
+                return configurationManager.GetConnectionString("DefaultConnectionString");
+            }
+        }
+    }
+}
--- src/Infrastructure/Persistence/ServiceRegistration.cs
+++ src/Infrastructure/Persistence/ServiceRegistration.cs
@@ -1,15 +1,16 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Persistence.Configuration;
 using Persistence.DataContext;
 namespace Persistence.ServiceRegistration
 {
     public static class ServiceRegistration
     {
-
         public static void AddPersistenceServices(this IServiceCollection services, IConfiguration configuration)
         {
-            services.AddDbContext<UniDbContext>(options => options.UseSqlServer(configuration.GetConnectionString("DefaultConnectionString")));
+            services.AddDbContext<UniDbContext>(options => options.UseSqlServer(ConnectionString.connectionString));
+            //services.AddDbContext<UniDbContext>(options => options.UseSqlServer(configuration.GetConnectionString("DefaultConnectionString")));            
         }
     }
 }
